e1a986eb1ec34d449f28379a8bfdd9105a5b9db2
Brenda Jow <brendaj0214@gmail.com>
2023-02-17 16:40:24 -0800

    completed code-challenge

--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,48 @@
 'use strict';
 
-class LinkedList {
+const { val } = require("cheerio/lib/api/attributes");
 
-}
+class LinkedList {
+    constructor() {
+        this.head = null;
+    };
+
+    insert(value) {
+        let current = this.head;
+        this.head = new Node(value);
+        this.head.next = current;
+        //one line this.head = new Node(value, this.head);
+    };
+
+    toString() {
+        let current = this.head;
+        let text = "";
+        while (current) {
+            text += ` { ${current.value} } -> `
+            current = current.next;
+        }
+        return text + "NULL";
+    }
+
+    includes(value) {
+        let curr = this.head;
+        while (curr.value != null) {
+            if (curr.value === value) {
+                return true;
+            } else {
+                return false;
+            }
+            curr = curr.next;
+        };
+    };
+
+};
+
+class Node {
+    constructor(value, next = null) {
+        this.value = value,
+            this.next = next;
+    };
+};
 
 module.exports = LinkedList;

03a4a5357daa6b5c770d01d2dfc2a7e696707338
Brenda <brendaj0214@gmail.com>
2023-03-03 15:05:10 -0800

    setup

---
+++ javascript/trees/binary-search-tree.js
@@ -0,0 +1,28 @@
+'use strict';
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class BinaryTree {
+  constructor(root){
+    this.root = root;
+  }
+
+  preOrder(){
+
+  }
+
+  inOrder(){
+
+  }
+
+  postOrder(){
+
+  }
+
+
+}

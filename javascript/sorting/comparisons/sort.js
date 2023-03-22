dcc228a8f0c50de9dbfb6a1c85531ddf03525ea5
Brenda <brendaj0214@gmail.com>
2023-03-22 16:50:24 -0700

    code challenge 28

--- javascript/sorting/comparisons/sort.js
+++ javascript/sorting/comparisons/sort.js
@@ -15,14 +15,21 @@ function compareYear(a, b) {
 }
 
 function sortTitle(arr) {
-  arr.sort((a, b) => compareTitle(a, b));
+  return arr.sort((a, b) => compareTitle(a, b));
 }
 
 
 function compareTitle(a, b) {
-  if(a.title.toLowerCase().includes() )
+
+  if(a.title.startsWith('The')) a.title.slice(4);
+  if(a.title.startsWith('An')) a.title.slice(3);
+  if(a.title.startsWith('A')) a.title.slice(2);
+  if(b.title.startsWith('The')) b.title = b.title.slice(4);
+  if(b.title.startsWith('An')) b.title = b.title.slice(3);
+  if(b.title.startsWith('A')) b.title = b.title.slice(2);
+
+  return a.title > b.title ? 1 : -1 ;
 }
 
-console.log(sortYear(Movies));
-
+console.log(sortTitle(Movies));
 module.exports = { sortYear, compareYear, sortTitle, compareTitle };
